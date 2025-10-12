0a2a644a57b564d4cb9376039ed3536cb5a4842f
Atharva Pardeshi <atharva.exe@gmail.com>
2025-10-12 18:54:27 +0530

    feat: handle saving recipes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import bcrypt from 'bcrypt'
+
 import User from '../models/user.js'
+import { authenticateToken } from "../utils/middleware.js"
 
 const router = express.Router()
 
@@ -19,6 +21,36 @@ router.post('/signup', async (req, res) => {
     const user = new User({ username, name, passwordHash })
     const savedUser = await user.save()
     res.status(201).json(savedUser)
+  } catch (err) {
+    res.status(400).json({ error: err.message })
+  }
+})
+
+// POST /api/users/save/:recipeId
+router.post('/save/:recipeId', authenticateToken, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id)
+
+    const recipeId = req.params.recipeId
+
+    if (user.savedRecipes.includes(recipeId)) {
+      return res.status(400).json({ error: 'Recipe already saved' })
+    }
+
+    user.savedRecipes.push(recipeId)
+    await user.save()
+
+    res.status(200).json({ message: 'Recipe saved', savedRecipes: user.savedRecipes })
+  } catch (err) {
+    res.status(400).json({ error: err.message })
+  }
+})
+
+// GET all saved recipes for the user
+router.get('/saved', authenticateToken, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).populate('savedRecipes')
+    res.json(user.savedRecipes)
   } catch (err) {
     res.status(400).json({ error: err.message })
   }
